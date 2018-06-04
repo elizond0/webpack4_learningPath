73f9770ae378af5711b3d3cd50eed08e35875565
elizondo <mamami1991@hotmail.com>
2018-06-05 00:53:57 +0800

    md文件排版修改

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 ## 1,webpack-dev-server启动方式(2种)
 
-1. /webpack.config.js  module.exports.devServer:{contentBase: path.join(__dirname, "dist")} => $ node_modules/.bin/webpack-dev-server
-2. /package.json  "scripts": {"server":"webpack-dev-server"} => $ npm run server
+1. /webpack.config.js : module.exports.devServer:{contentBase: path.join(__dirname, "dist")} => $ node_modules/.bin/webpack-dev-server
+2. /package.json : "scripts": {"server":"webpack-dev-server"} => $ npm run server
 
 ## 2,html打包插件:html-webpack-plugin
 
 1. $ npm install -s-d html-webpack-plugin
-2. /webpack.config.js  const htmlPlugin= require('html-webpack-plugin')  详情见配置文件
+2. /webpack.config.js : const htmlPlugin= require('html-webpack-plugin')  详情见配置文件
 
 ## 3,css-loader:style-loader(处理css文件中的url) css-loade(将css插入到页面中style标签)
 
-1. $ npm install -s-d style-loader css-loader 
-2. /webpack.config.js  详情见配置文件
+1. $ npm install -s-d style-loader css-loader
+2. /webpack.config.js : 详情见配置文件
 
 ## 4,css中的图片处理:file-loader、url-loader
 
@@ -25,20 +25,20 @@
 ## 5,CSS分离与图片路径处理(webpack官方并不建议这样作，他们认为CSS就应该打包到JavasScript当中以减少http的请求数。看业务需求)
 
 1. $ npm install -s-d extract-text-webpack-plugin
-2. /webpack.config.js  设置Plugins  修改原来我们的style-loader和css-loader  详情见配置文件
-3. webpack目前已升级V4,而相应的extract-text-webpack-plugin并不能兼容,因此安装是需要使用测试版 npm install -s-d extract-text-webpack-plugin@next,临时解决还需要继续观察
+2. /webpack.config.js : 设置Plugins  修改原来我们的style-loader和css-loader  详情见配置文件
+3. webpack目前已升级V4,而相应的extract-text-webpack-plugin并不能兼容,因此安装是需要使用测试版 : npm install -s-d extract-text-webpack-plugin@next,临时解决还需要继续观察
 
 ## 6,修改打包后图片路径;处理HTML中的图片(html-withimg-loader)
 
-1. /webpack.config.js 修改配置module.rules下的url-loader选项的outputPath
-2. $ npm install html-withimg-loader -s-d 国人开发,api易懂
-3. /webpack.config.js 配置loader 详情见配置文件
+1. /webpack.config.js : 修改配置module.rules下的url-loader选项的outputPath
+2. $ npm install html-withimg-loader -s-d (国人开发,api易懂)
+3. /webpack.config.js : 配置loader 详情见配置文件
 
 ## 7,Less文件的打包和分离
 
 1. $ npm install -s-d less less-loader
-2. /webpack.config.js loader配置见文件
-3. index.js中import less文件
+2. /webpack.config.js - loader配置见文件
+3. index.js中import - less文件
 
 ## 8,sass文件的打包和分离与less类似(node-sass sass-loader)
 
@@ -47,11 +47,4 @@
 1. PostCSS是一个CSS的处理平台,自动处理CSS3属性前缀是其中的一个功能。
 2. $ npm install -s-d postcss-loader autoprefixer
 3. 新建文件postcss.config.js
-4. /webpack.config.js loader配置
-
-
-
-
-
-
-
+4. /webpack.config.js - loader配置
--- dist/entry.js
+++ dist/entry.js
@@ -1,1 +1,97 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:r})},n.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="http://localhost:9000/",n(n.s=0)}({0:function(e,t,n){"use strict";n.r(t);n(7);document.getElementById("title").innerHTML="index.js",console.log("index23444")},7:function(e,t){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "http://localhost:9000/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.css":
+/*!***********************!*\
+  !*** ./src/index.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:4)\\nYou may need an appropriate loader to handle this file type.\\n| body{\\r\\n|     color:red;\\r\\n| }\\r\");\n\n//# sourceURL=webpack:///./src/index.css?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./src/index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\r\n// import css from './less.less'\r\ndocument.getElementById('title').innerHTML='index.js';\r\nconsole.log(\"index23444\");\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
